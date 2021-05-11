147f5a2cf21d5973e0e36169aea3fda4d704beb6
lvhaosir <lvhao@umeox.com>
2021-05-11 19:41:22 +0800

    Initial commit

---
+++ src/cn/lvhaosir/Main.java
@@ -0,0 +1,13 @@
+package cn.lvhaosir;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Map<String, String> map = new HashMap<>();
+        map.put("1","1");
+    }
+}
